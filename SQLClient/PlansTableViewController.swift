a31b6ea2012040a395d96cfca876056556d3382d
rhit-wangy51 <wangy51@rose-hulman.edu>
2022-04-29 13:04:41 -0400

    update main again

--- SQLClient/PlansTableViewController.swift
+++ SQLClient/PlansTableViewController.swift
@@ -7,11 +7,7 @@
 //
 
 import UIKit
-
-class PlanCell: UITableViewCell{
-    
-}
-
+import Pods_SQLClient
 
 class PlansTableViewController: UITableViewController {
 
@@ -19,12 +15,12 @@ class PlansTableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.clearsSelectionOnViewWillAppear = false.
         self.navigationItem.rightBarButtonItem = self.editButtonItem
         
 //        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action:  #selector(showAddPlanDialog))
+        
+        
 
     }
 
@@ -102,77 +98,20 @@ class PlansTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Prepare to go")
+//        if segue.identifier == kShowPlanDetailTableViewSegue{
+//
+//            print("Go to plan detail")
+//        }
     }
-    */
     
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.edit,
-//                                                                 target: self,
-//                                                                 action: #selector(showEditQuoteDialog))
-////        updateView()
-//
-//    }
-//
-//
-//
-//        //if viewDidLoad crashes, I could do this
-////    override func viewDidAppear(_ animated: Bool) {
-////        super.viewDidAppear(animated)
-////        updateView()
-////    }
-//
-////    func updateView(){
-////        quoteLabel.text = movieQuote.quote
-////        movieLabel.text = movieQuote.movie
-////    }
-//
-//    @objc func showEditQuoteDialog(){
-//
-//
-//        let alertController = UIAlertController(title: "Edit movie quote",
-//                                                message: "",
-//                                                preferredStyle: UIAlertController.Style.alert)
-//
-////        alertController.addTextField { textField in
-////            textField.placeholder = "Quote"//the grey word
-////            textField.text = self.movieQuote.quote
-////        }
-////
-////        alertController.addTextField { textField in
-////            textField.placeholder = "Movie"//the grey word
-////            textField.text = self.movieQuote.movie
-////        }
-//
-//        //create an action and add it to the controller
-//        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) { action in
-//            print("You pressed cancel")
-//        }
-//        alertController.addAction(cancelAction)
-//
-//        //positive button
-//        let editQuoteAction = UIAlertAction(title: "Edit quote", style: UIAlertAction.Style.default) { action in
-//
-////            let quoteTextField = alertController.textFields![0] as UITextField
-////            let movieTextField = alertController.textFields![1] as UITextField
-////            print("Quote: \(quoteTextField.text!)")
-////            print("Movie: \(movieTextField.text!)")
-////
-////            self.movieQuote.quote = quoteTextField.text!
-////            self.movieQuote.movie = movieTextField.text!
-////            self.updateView()
-//
-//        }
-//        alertController.addAction(editQuoteAction)
-//
-//        present(alertController, animated: true)//to show the thing
-//    }
+    
 }
